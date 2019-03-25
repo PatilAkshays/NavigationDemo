25837cef4e42fb5044e5337ea15525d032d25746
Treel Mobility <treel@Treels-MacBook-Air.local>
2019-03-25 17:35:08 +0530

    initial Commit

--- NavigationDemo/NewViewController.swift
+++ NavigationDemo/NewViewController.swift
@@ -9,11 +9,54 @@
 import UIKit
 
 class NewViewController: UIViewController {
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
 
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+
+    }
+    @objc func keyboardWillShow(notification: Notification) {
+        if ((notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue) != nil {
+            print("notification: Keyboard will show")
+            self.navigationController?.setNavigationBarHidden(true, animated: true)
+            searchBar.showsCancelButton = true
+            
+//            if self.view.frame.origin.y == 0{
+//                self.view.frame.origin.y -= keyboardSize.height
+//            }
+        }
+        
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        if ((notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue) != nil {
+            self.navigationController?.setNavigationBarHidden(false, animated: true)
+            searchBar.showsCancelButton = false
+            
+//            if self.view.frame.origin.y != 0 {
+//                self.view.frame.origin.y += keyboardSize.height
+//            }
+        }
+    }
+    
     @IBAction func backbuttonAc(_ sender: Any) {
         dismiss(animated: true)
     }
     
+    @IBAction func cancelB(_ sender: Any) {
+        dismiss(animated: true)
+        NotificationCenter.default.post(name: .showHomeScreen, object: nil);
+    }
     @IBAction func backB(_ sender: Any) {
         dismiss(animated: true)
         NotificationCenter.default.post(name: .showHomeScreen, object: nil);
@@ -29,4 +72,44 @@ class NewViewController: UIViewController {
     }
     */
 
+}
+
+extension NewViewController : UITableViewDataSource,UITableViewDelegate{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return 10;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row < 3{
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell\(indexPath.row)")
+            return cell!;
+        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        return cell!;
+    }
+    
+}
+extension NewViewController : UISearchBarDelegate{
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+       
+
+    }
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+       
+        searchBar.resignFirstResponder()
+    }
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+     return true
+    }
+    func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
+       return true
+
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        searchBar.resignFirstResponder()
+    }
 }
